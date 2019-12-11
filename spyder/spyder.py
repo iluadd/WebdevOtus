27f0e3ed4706975e4f6b61c65aa9552f01a98e6b
iluadd <ildozh@gmail.com>
2019-12-11 22:42:11 +0300

    added exceptions, attr checks,  recursion level

--- spyder/spyder.py
+++ spyder/spyder.py
@@ -5,8 +5,22 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_links(page=None, numlinks=None, curent_num=1, rec=None):
-    page = requests.get(page)
+class NumLinksExceeded(Exception):
+    pass
+
+
+def get_links(page=None, numlinks=None, curent_num=1, rec_l=None):
+
+    try:
+        page = requests.get(page)
+
+    except requests.exceptions.MissingSchema:
+        print('Invalid URL')
+        raise
+
+    except Exception as e:
+        raise(e)
+
     soup = BeautifulSoup(page.content, features="html.parser")
 
     for num, link in enumerate(soup.findAll('a',
@@ -14,23 +28,35 @@ def get_links(page=None, numlinks=None, curent_num=1, rec=None):
                                start=curent_num):
 
         if num > numlinks:
-            raise(Exception('End of recursive search'))
+            raise NumLinksExceeded()
 
         a_link = link.get('href')
-        print(f'{num} : {a_link}')
+        print(f'{num} : {a_link} ')
 
-        if rec:
+        if rec_l > 0:
             get_links(page=a_link, numlinks=numlinks,
-                      curent_num=num+1, rec=True)
+                      curent_num=num+1, rec_l=rec_l-1)
+
+    return True
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('rootpage', type=str, nargs=1, help='root page')
-    parser.add_argument('numlinks', type=int, nargs=1, help='number of links')
+    parser.add_argument('rootpage', type=str, help='root page')
+    parser.add_argument('numlinks', type=int, help='number of links')
+    parser.add_argument('-rl', '--recur_level', type=int, dest='recursive_level')
+    parser.set_defaults(recursive_level=0)
 
     args = parser.parse_args()
-    rootpage = args.rootpage[0]
-    numlinks = args.numlinks[0]
+    rootpage = args.rootpage
+    numlinks = args.numlinks
+    recursive_level = args.recursive_level
 
-    get_links(page=rootpage, numlinks=numlinks, rec=True)
+    if numlinks < 1:
+        print('provide more then 0 number of links')
+        raise Exception('Not enough links provided')
+
+    try:
+        get_links(page=rootpage, numlinks=numlinks, rec_l=recursive_level)
+    except NumLinksExceeded:
+        print('We are done !')
